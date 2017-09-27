54903b84f1adfb3f9305ecc0d0cfd7a8a8b18431
Fuyang Liu <fli@tradeshift.com>
2017-09-27 14:37:25 +0200

    Set up init script

---
+++ init_python3_ml_env.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+PYTHON_ENV_NAME=p3ml-venv
+
+# pip install virtualenv
+
+virtualenv -p python3.6 $PYTHON_ENV_NAME
+
+echo "source $(pwd)/$PYTHON_ENV_NAME/bin/activate" > .env
+
+source $(pwd)/$PYTHON_ENV_NAME/bin/activate # activate the local python environment
+
+pip install jupyter
+pip install scikit-learn
+pip install matplotlib
+pip install pandas
+pip install scipy
